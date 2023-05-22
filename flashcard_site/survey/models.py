1036daa0aa4e93cc50f5e2e1fe09ffedc45f32b4
Jacob G-W <jacoblevgw@gmail.com>
2023-05-22 19:47:05 -0400

    start implementing the algo

--- flashcard_site/survey/models.py
+++ flashcard_site/survey/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import datetime
 import json
 
-# Create your models here.
+CARD_TYPE_NEW = 0
+CARD_TYPE_LRN = 1
+CARD_TYPE_REV = 2
+CARD_TYPE_RLN = 3
+
+QUEUE_TYPE_NEW = 0
+QUEUE_TYPE_LRN = 1
+QUEUE_TYPE_REV = 2
 
 class User(AbstractUser):
 	pass
@@ -10,8 +18,14 @@ class Card(models.Model):
     belongs = models.ForeignKey(User, on_delete=models.CASCADE)
     front = models.CharField(max_length=800)
     back = models.CharField(max_length=800)
-    date_next = models.DateField(auto_now_add=True)
+    date_next = models.DateField(default=datetime.date.today)
     date_next.editable=True
+    easiness = models.FloatField(default=2.5)  # Initial value for easiness factor in SM2 algorithm is 2.5
+    interval = models.IntegerField(default=1)  # Initial interval is 1 day
+    repetitions = models.IntegerField(default=0)  # Initially, the card has not been reviewed, so repetitions is 0
+    card_type = models.IntegerField(default=CARD_TYPE_NEW) # Initially, the card is new
+    queue_type = models.IntegerField(default=QUEUE_TYPE_NEW)
+
 
     def __str__(self):
         return f'{self.front}/{self.back}/next: {self.date_next}'
--- flashcard_site/survey/views.py
+++ flashcard_site/survey/views.py
@@ -1,35 +1,64 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 import datetime
 import json
 
-from .models import Card
-
-# Create your views here.
+from .models import Card, CARD_TYPE_NEW, CARD_TYPE_LRN, CARD_TYPE_REV, CARD_TYPE_RLN, QUEUE_TYPE_NEW, QUEUE_TYPE_LRN, QUEUE_TYPE_REV
 def index(request):
-	return HttpResponse("At Survey Index TODO")
+    return HttpResponse("At Survey Index TODO")
 
-@login_required
-def get_cards(request):
-    user = request.user
-    #TODO I also want it to trigger if date_next < today 
-    reviews_for_today = Card.objects.filter(date_next=datetime.date.today())
-    if len(reviews_for_today) > 0:
+NEW_ADDED_EVERY_DAY = 5
+
+def get_card_from_cards(cards):
+    if len(cards) > 0:
         review = reviews_for_today[0]
         id = review.id
         front = review.front
         back = review.back
-        return HttpResponse(json.dumps({'id': id, 'front': front, 'back': back}), content_type='application/json')
+        return json.dumps({'id': id, 'front': front, 'back': back})
+    else:
+        return None
+
+@login_required
+def get_cards(request):
+    user = request.user
+    # first do things in the QUEUE_TYPE_LRN
+    lrn_for_today = Card.objects.filter(date_next__lte=datetime.date.today(), queue_type = QUEUE_TYPE_LRN)
+    lrn_for_today_card = get_card_from_cards(lrn_for_today)
+    if lrn_for_today_card != None:
+        return HttpResponse(lrn_for_today, content_type='application/json')
+    # next do things in the QUEUE_TYPE_RLN
+    rln_for_today = Card.objects.filter(date_next__lte=datetime.date.today(), queue_type = QUEUE_TYPE_RLN)
+    rln_for_today_card = get_card_from_cards(lrn_for_today)
+    if rln_for_today_card != None:
+        return HttpResponse(rln_for_today, content_type='application/json')
+
 @login_required
 def submit_card(request):
     if not request.POST:
         return HttpResponse('/submit_card needs a POST request')
     req = json.parse(request.POST['body'])
-    ease = req['ease']
+    quality = req['quality']
     id = req['id']
     card = Card.objects.filter(id=req['id'], user=request.user)
     if len(card) != 1:
         return HttpResponse("stop trying to hack")
     card = card[0]
-    # TODO business logic
+    if quality < 3:
+        card.interval = 0 # do it again today
+        card.repititions = 0 # reset the streak
+    else:
+        if card.repititions == 0:
+            card.interval = 1
+        elif card.repititions == 1:
+            card.interval == 6 # TODO is 6 correct?
+        else:
+                card.interval = ceil(card.interval * card.easinessA)
+
+        card.repititions += 1
+    card.easiness += 0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02)
+    if card.easiness < 1.3:
+        card.easiness = 1.3
+    review_date = datetime.date.today() + datetime.timedelta(interval)
     return HttpResponse(f'analyzed {card}')
