90d685199c37e6bee2e8f83d5c0fa2953875e9ef
Jacob G-W <jacoblevgw@gmail.com>
2023-05-22 22:10:55 -0400

    more update with new cards i need to do a cronjob

--- flashcard_site/survey/models.py
+++ flashcard_site/survey/models.py
@@ -12,20 +12,30 @@ QUEUE_TYPE_NEW = 0
 QUEUE_TYPE_LRN = 1
 QUEUE_TYPE_REV = 2
 
+NEW_ADDED_EVERY_DAY = 5
+
+
 class User(AbstractUser):
-	pass
+    new_cards_added_today = models.IntegerField(
+        default=NEW_ADDED_EVERY_DAY
+    )  # think of better way to do this
+
+
 class Card(models.Model):
     belongs = models.ForeignKey(User, on_delete=models.CASCADE)
     front = models.CharField(max_length=800)
     back = models.CharField(max_length=800)
     date_next = models.DateField(default=datetime.date.today)
-    date_next.editable=True
-    easiness = models.FloatField(default=2.5)  # Initial value for easiness factor in SM2 algorithm is 2.5
+    date_next.editable = True
+    easiness = models.FloatField(
+        default=2.5
+    )  # Initial value for easiness factor in SM2 algorithm is 2.5
     interval = models.IntegerField(default=1)  # Initial interval is 1 day
-    repetitions = models.IntegerField(default=0)  # Initially, the card has not been reviewed, so repetitions is 0
-    card_type = models.IntegerField(default=CARD_TYPE_NEW) # Initially, the card is new
+    repetitions = models.IntegerField(
+        default=0
+    )  # Initially, the card has not been reviewed, so repetitions is 0
+    card_type = models.IntegerField(default=CARD_TYPE_NEW)  # Initially, the card is new
     queue_type = models.IntegerField(default=QUEUE_TYPE_NEW)
 
-
     def __str__(self):
-        return f'{self.front}/{self.back}/next: {self.date_next}'
+        return f"{self.front}/{self.back}/next: {self.date_next}"
--- flashcard_site/survey/views.py
+++ flashcard_site/survey/views.py
@@ -4,61 +4,98 @@ from django.db.models import Q
 import datetime
 import json
 
-from .models import Card, CARD_TYPE_NEW, CARD_TYPE_LRN, CARD_TYPE_REV, CARD_TYPE_RLN, QUEUE_TYPE_NEW, QUEUE_TYPE_LRN, QUEUE_TYPE_REV
+from .models import (
+    Card,
+    CARD_TYPE_NEW,
+    CARD_TYPE_LRN,
+    CARD_TYPE_REV,
+    CARD_TYPE_RLN,
+    QUEUE_TYPE_NEW,
+    QUEUE_TYPE_LRN,
+    QUEUE_TYPE_REV,
+    NEW_ADDED_EVERY_DAY,
+)
+
+
 def index(request):
     return HttpResponse("At Survey Index TODO")
 
-NEW_ADDED_EVERY_DAY = 5
 
 def get_card_from_cards(cards):
     if len(cards) > 0:
-        review = reviews_for_today[0]
+        review = cards[0]
         id = review.id
         front = review.front
         back = review.back
-        return json.dumps({'id': id, 'front': front, 'back': back})
+        card_type = review.card_type
+        queue_type = review.queue_type
+        return json.dumps(
+            {
+                "id": id,
+                "front": front,
+                "back": back,
+                "card_type": card_type,
+                "queue_type": queue_type,
+            }
+        )
     else:
         return None
+
 
 @login_required
 def get_cards(request):
     user = request.user
-    # first do things in the QUEUE_TYPE_LRN
-    lrn_for_today = Card.objects.filter(date_next__lte=datetime.date.today(), queue_type = QUEUE_TYPE_LRN)
+
+    lrn_for_today = Card.objects.filter(
+        date_next__lte=datetime.date.today(), queue_type=QUEUE_TYPE_LRN
+    )
     lrn_for_today_card = get_card_from_cards(lrn_for_today)
     if lrn_for_today_card != None:
-        return HttpResponse(lrn_for_today, content_type='application/json')
-    # next do things in the QUEUE_TYPE_RLN
-    rln_for_today = Card.objects.filter(date_next__lte=datetime.date.today(), queue_type = QUEUE_TYPE_RLN)
-    rln_for_today_card = get_card_from_cards(lrn_for_today)
-    if rln_for_today_card != None:
-        return HttpResponse(rln_for_today, content_type='application/json')
+        return HttpResponse(lrn_for_today_card, content_type="application/json")
+
+    rev_for_today = Card.objects.filter(
+        date_next__lte=datetime.date.today(), queue_type=QUEUE_TYPE_REV
+    )
+    rev_for_today_card = get_card_from_cards(lrn_for_today)
+    if rev_for_today_card != None:
+        return HttpResponse(rev_for_today_card, content_type="application/json")
+
+    if user.new_cards_added_today > 5:
+        rev_for_today = Card.objects.filter(
+            date_next__lte=datetime.date.today(), queue_type=QUEUE_TYPE_NEW
+        )
+        rev_for_today_card = get_card_from_cards(lrn_for_today)
+        if rev_for_today_card != None:
+            return HttpResponse(rev_for_today_card, content_type="application/json")
+    return HttpResponse(json.dumps({"id": None}), content_type="application/json")
+
 
 @login_required
 def submit_card(request):
     if not request.POST:
-        return HttpResponse('/submit_card needs a POST request')
-    req = json.parse(request.POST['body'])
-    quality = req['quality']
-    id = req['id']
-    card = Card.objects.filter(id=req['id'], user=request.user)
+        return HttpResponse("/submit_card needs a POST request")
+    req = json.parse(request.POST["body"])
+    quality = req["quality"]
+    id = req["id"]
+    card = Card.objects.filter(id=req["id"], user=request.user)
     if len(card) != 1:
         return HttpResponse("stop trying to hack")
     card = card[0]
     if quality < 3:
-        card.interval = 0 # do it again today
-        card.repititions = 0 # reset the streak
+        card.interval = 0  # do it again today
+        card.repititions = 0  # reset the streak
     else:
         if card.repititions == 0:
             card.interval = 1
         elif card.repititions == 1:
-            card.interval == 6 # TODO is 6 correct?
+            card.interval == 6  # TODO is 6 correct?
         else:
-                card.interval = ceil(card.interval * card.easinessA)
+            card.interval = ceil(card.interval * card.easinessA)
 
         card.repititions += 1
     card.easiness += 0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02)
     if card.easiness < 1.3:
         card.easiness = 1.3
     review_date = datetime.date.today() + datetime.timedelta(interval)
-    return HttpResponse(f'analyzed {card}')
+    # TODO graduate card and decrement user.new_cards_added_today
+    return HttpResponse(f"analyzed {card}")
