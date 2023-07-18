ec34befc0f66fdb9084fceeeb326d88e5c4a5cad
Jacob G-W <jacoblevgw@gmail.com>
2023-07-18 14:44:26 -0400

    i got the algorithm to work (generate1.py)

--- flashcard_site/ai/generate1.py
+++ flashcard_site/ai/generate1.py
@@ -5,8 +5,10 @@ import sys
 from prompt_genfact import *
 import time
 
-INPUT_paragraphs = sys.stdin.readlines()
-cards = []
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+USE_GPT_4 = True
+MODEL = "gpt-4" if USE_GPT_4 else "gpt-3.5-turbo-16k"
 
 
 def extract_json_objects(string):
@@ -26,15 +28,8 @@ def extract_json_objects(string):
     return json_objects
 
 
-def get_cards_from_paragraph(paragraph, context):
-    CURR_PROMPT = f"""
-    {f"Here is the previous paragraph: {context}" if context != None else ""}
-    Please now make cards on this paragraph:
-    ===
-    {paragraph}
-    ===
-    Remember to make the cards context free. They should be able to be completed even if the user has not read the text. They should not reference 'it' or 'that' or anything ambigious. Use names and specifics. The use should be able to do them in any order. BE SPECIFIC!!!
-    """
+def get_facts_from_paragraph(paragraph):
+    CURR_PROMPT = paragraph
     messages = [
         {"role": "system", "content": SYSTEM},
         {"role": "user", "content": USER1},
@@ -43,17 +38,35 @@ def get_cards_from_paragraph(paragraph, context):
         {"role": "assistant", "content": ASS2},
         {"role": "user", "content": USER3},
         {"role": "assistant", "content": ASS3},
+        # {"role": "user", "content": USERFACT1_WRONG},
+        # {"role": "assistant", "content": ASSFACT1_WRONG},
+        # {"role": "user", "content": USERFACT2_WRONG + CURR_PROMPT},
         {"role": "user", "content": CURR_PROMPT},
     ]
-    openai.api_key = os.getenv("OPENAI_API_KEY")
 
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
+        model=MODEL,
         messages=messages,
         temperature=1.3,
         max_tokens=word_count * 9,
     )
 
+    message = completion.choices[0].message
+    time.sleep(3)
+    return message["content"]
+
+
+def get_cards_from_facts(facts):
+    messages = [
+        {"role": "system", "content": SYSTEM_CARD},
+        {"role": "user", "content": facts},
+    ]
+    completion = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=messages,
+        temperature=1.3,
+        max_tokens=word_count * 9,
+    )
     message = completion.choices[0].message
     time.sleep(3)
     return extract_json_objects(message["content"])
@@ -63,20 +76,19 @@ def debug(*args, **kwargs):
     print("DEBUG ", *args, file=sys.stderr, **kwargs)
 
 
-PROCESSED_paragraphs = [para.strip() for para in INPUT_paragraphs if para.strip()]
+if __name__ == "__main__":
+    INPUT_paragraphs = sys.stdin.readlines()
+    cards = []
+    PROCESSED_paragraphs = [para.strip() for para in INPUT_paragraphs if para.strip()]
 
-for i, paragraph in enumerate(PROCESSED_paragraphs):
-    if i == 0:
-        context = PROCESSED_paragraphs[i - 1]
-        debug("skipping context because its the first card")
-    else:
-        context = None
-    word_count = len(paragraph.split())
-    if word_count < 20:
-        debug("skipping paragraph because its too short:", paragraph)
-        continue
-    cs = get_cards_from_paragraph(paragraph, context)
-    debug(json.dumps(cs))
-    cards.extend(cs)
+    for i, paragraph in enumerate(PROCESSED_paragraphs):
+        word_count = len(paragraph.split())
+        if word_count < 20:
+            debug("skipping paragraph because its too short:", paragraph)
+            continue
+        facts = get_facts_from_paragraph(paragraph)
+        debug("facts:", facts)
+        cs = get_cards_from_facts(facts)
+        cards.extend(cs)
 
-print(json.dumps(cards))
+        print(json.dumps(cards))
--- flashcard_site/ai/prompt_genfact.py
+++ flashcard_site/ai/prompt_genfact.py
@@ -1,3 +1,150 @@
+SYSTEM_CARD = """
+You are a tool designed to make Anki flashcards.Your task is to create flashcards from informational text that you will get. Here are some rules to do it:
+
+Minimum information principle:
+Feel free to make a LOT of flashcards for an item, but don't put too much information in the answer.
+
+Determine the subject of the flashcards and put it before the question to give the reader context.
+
+This is bad:
+Q: What are the characteristics of the Dead Sea?
+A: Salt lake located on the border between Israel and Jordan. Its shoreline is the lowest point on the Earth's surface, averaging 396 m below sea level. It is 74 km long. It is seven times as salty (30% by volume) as the ocean. Its density keeps swimmers afloat. Only simple organisms can live in its saline waters
+
+This is very good, each piece of information is its own question:
+Q: Dead Sea: Where is the Dead Sea located?
+A: on the border between Israel and Jordan
+
+Q: Dead Sea: What is the lowest point on the Earth's surface?
+A: The Dead Sea shoreline
+
+Q: Dead Sea: What is the average level on which the Dead Sea is located?
+A: 400 meters (below sea level)
+
+Q: Dead Sea: How long is the Dead Sea?
+A: 70 km
+
+Q: Dead Sea: How much saltier is the Dead Sea as compared with the oceans?
+A: 7 times
+
+Q: Dead Sea: What is the volume content of salt in the Dead Sea?
+A: 30%
+
+Q: Dead Sea: Why can the Dead Sea keep swimmers afloat?
+A: due to high salt content
+
+Q: Dead Sea: Why is the Dead Sea called Dead?
+A: because only simple organisms can live in it
+
+Q: Dead Sea: Why only simple organisms can live in the Dead Sea?
+A: because of high salt content
+
+Another principle to follow is to avoid sets:
+
+Ill-formulated knowledge - Sets are unacceptable!
+
+Q: European Union: What countries belong to the European Union (2002)?
+A: Austria, Belgium, Denmark, Finland, France, Germany, Greece, Ireland, Italy, Luxembourg, the Netherlands, Portugal, Spain, Sweden, and the United Kingdom
+Well-formulated knowledge - Converting a set into a meaningful listing
+
+Q: European Union: Which country hosted a meeting to consider the creation of a European Community of Defence in 1951?
+A: France
+
+Q: European Union: Which countries apart from France joined the European Coal and Steel Community in 1952?
+A: Germany, Italy and the Benelux
+
+Q: European Union: What countries make up the Benelux?
+A: Belgium, Luxembourg, and the Netherlands
+
+Q: European Union: Whose membership did Charles de Gaulle oppose in the 1960s?
+A: that of UK
+
+Q: European Union: Which countries joined the EEC along the UK in 1973?
+A: Ireland and Denmark
+
+Q: European Union: Which country joined the EEC in 1981?
+A: Greece
+
+Q: European Union: What was the historic course of expansion of the European Union membership?
+A: (1) France and (2) Germany, Italy and the Benelux, (3) UK and (4) Ireland and Denmark, (5) Greece, (6) Spain and Portugal and (7) Austria, Sweden and Finland
+
+Only use this if you have to:
+Enumerations are also an example of classic items that are hard to learn. They are still far more acceptable than sets. Avoid enumerations wherever you can. If you cannot avoid them, deal with them using cloze deletions (overlapping cloze deletions if possible). Learning the alphabet can be a good example of an overlapping cloze deletion:
+Hard to learn item
+
+Q: Alphabet: What is the sequence of letters in the alphabet?
+A: abcdefghijklmnopqrstuvwxyz
+Easy to learn items
+
+Q: Alphabet: What three letters does the alphabet begin with?
+A: ABC
+
+Q: Alphabet: Fill out the missing letters of the alphabet A ... ... ... E
+A: B, C, D
+
+Q: Alphabet: Fill out the missing letters of the alphabet B ... ... ... F
+A: C, D, E
+
+Less optimum item: cloze deletion that is too wordy
+
+Q: Aldus invented desktop publishing in 1985 with PageMaker. Aldus had little competition for years, and so failed to improve. Then Denver-based ... blew past. PageMaker, now owned by Adobe, remains No. 2
+A: Quark
+Better item: fewer words will speed up learning
+
+Q: Aldus invented desktop publishing with PageMaker but failed to improve. It was soon outdistanced by ...
+A: Quark
+
+Or better:
+
+Q: PageMaker failed to improve and was outdistanced by ...
+A: Quark
+
+Or better:
+
+Q: PageMaker lost ground to ...
+A: Quark
+
+
+Another principle: be redundant. Redundancy means having lots of flashcards that all attack different angles of a single piece of information.
+Each one should follow minimum information, but there can be a lot of them.
+
+Principle: Avoid yes or no questions. Instead of yes or no, ask about the topic. Instead of Q: Did Robert McCloskey win the Caldecott Honor in 1949? A: Yes
+Q: What medal did Robert McCloskey win in 1949? Q: Caldecott Honor
+Yes and no questions are really bad because they are really easy to guess.
+
+Questions should not refer to previous questions or even the text.
+These are bad: 
+Q: Which international organizations is Ukraine a founding member of?
+A: United Nations 
+Q: Which international organizations is Ukraine a member of?
+A: Council of Europe, World Trade Organization, OSCE 
+Q: What other international organizations has Ukraine expressed interest in joining?
+A: European Union and NATO
+
+They should *not* be ambigious and a question should not depend on other questions to understand it.
+Instead, questions should be more specific so that there is only one right answer. This is *really* important.
+
+When you make the flashcards, use the process of chain-of-thought reasoning.
+
+Finally, and this is really important: the question and answer pair that you give should be *context independent* from the text. The user will see the questions out of order and not with the text, so don't include ANYTHING that references the text in the questions.
+Instead of saying "Who does ...?" say "Who does ... in the [the name of the story]?" Never use "Who does ... in the novel?". Always use the actual name of the novel.
+For every line of the input, give one (or two) questions. Give it in the format of {"question": "text of the question", "answer": "text of the answer"} with every question/answer pair on a new line.
+Here is an example of how it should be done:
+INPUT:
+Juno and the Paycock (1924) is a play written by Seán O’Casey.
+The play is set during the Irish Civil War.
+The main character is Jack Boyle, known as "Captain", who is an alcoholic.
+Jack's family consists of his wife Juno, daughter Mary, and son Johnny.
+Charles Bentham brings news of a family member's death and a large inheritance for Jack.
+OUTPUT:
+{"question": "Author: Juno and the Paycock", "answer": "Seán O'Casey"}
+{"question": "Setting: Juno and the Paycock", "answer": "Irish Civil War"}
+{"question": "Main Character: Juno and the Paycock", "answer": "Jack Boyle"}
+{"question": "Juno and the Peacock: what is Jack known as", "answer": "Captain"}
+{"question": "Juno and the Peacock: Disease that Jack Boyle has", "answer": "alcoholism"}
+{"question": "Juno and the Peacock: 3 family members of Jack", "answer": "Juno, Mary, and Johnny"}
+{"question": "Juno and the Paycock: Who brings news of a family member's death and inheritance to Jack", "answer": "Charles Bentham"}
+"""
+
 SYSTEM = """
 You are an AI designed to take a paragraph and turn it into CONTEXT INDEPENDENT facts.
 If the paragraph references something that is not in it (maybe using words like 'these' 'this' or 'it'), just ignore it.
@@ -36,4 +183,41 @@ Plato's father was Ariston.
 Plato's father was said to be an ancestor of the last king of Athens.
 The last king of athens was Crodus.
 Plato's mother was Perictione.
+Most of what is known about Plato's early life comes from his writings.
+Plato's father died when Plato was young.
+It is very likely that Plato knew Socrates from early childhood.
+"""
+USER3 = """
+Aboriginal Australians comprise various indigenous peoples who lived in Australia before European settlers arrived. Traditionally, Aboriginal peoples believed that Australia was created during a mythical period called the Dreamtime, during which beings like the Rainbow Serpent ruled. When the British arrived in Australia, they persecuted Aboriginal people and seized their land in the Frontier Wars. In the 1900s, many Aboriginal children were taken from their families, becoming known as the Stolen Generations. While European mistreatment of Aboriginal people is recognized on National Sorry Day, they still face discrimination, including higher rates of poverty and incarceration. Aboriginal people are distinct from the Torres Strait Islanders, who live north of mainland Australia. 
+"""
+ASS3 = """
+Aboriginal Australians lived in Australia before European settlers arrived.
+Aboriginal peoples believed that Australia was created during a mythical period called the Dreamtime.
+During the Dreamtime, Aboriginals believed that creatures like the Rainbow Serpent ruled.
+When the British arrived in Australia, waged the Frontier Wars against the Aboriginals.
+During the Frontier Wars, the British persecuted the Aboriginals and seized their land.
+In the 1900s, many Aboriginal children were taken from their families.
+The Aboriginal children that were taken from their families were called the Stolen Generations.
+European mistreatement of Aboriginals is recognized on National Sorry Day.
+Aboriginals still face higher rates of poverty and incarceration.
+Aboriginal people are distinct from the Torres Strait Islanders
+Torres Strait Islanders who live north of mainland Australia.
+"""
+
+USERFACT1_WRONG = """
+A Modest Proposal (1729) is a satirical essay by Jonathan Swift. In the essay, Swift proposes alleviating the “burden” created by the children of the poor by using the children as food. Swift cites a conversation with “a very knowing American” who notes that “a young healthy child, well nursed, is at a year old a most delicious, nourishing, and wholesome food.” Swift continues this satire by offering advice involving the timing of availability (which would be greatest nine months after Lent), and notes that the flesh of children would be appropriate for landlords, since they “have already devoured most of the parents.” Swift couches his actual suggestions for alleviating poverty—such as taxing absentees—in a list of ideas he has considered and rejected as impossible to implement.
+"""
+
+ASSFACT1_WRONG = """
+Jonathan Swift wrote a satirical essay named "A Modest Proposal" in 1729.
+In "A Modest Proposal", Swift suggested using children of the poor as food as a method to alleviate burden.
+The essay quotes a conversation with "a very knowing American" stating that a young healthy child is nutritious food.
+Swift suggests that children would be abundantly available for eating nine months after Lent.
+Swift implies landlords to be appropriate consumers of children as they "have already devoured most of the parents."
+Actual suggestions made by Swift to alleviate poverty involve taxing absentees.
+"""
+
+USERFACT2_WRONG = """
+That is incorrect. When you say "The essay quotes", the user won't know about which essay you are talking about. The lines need to be independent. When you say "Swift suggests" you need to say "Swift suggests in "A Modest Proposal"". I want you to try again, and please make every line a fact that could be understood without reading the other lines:
+
 """
